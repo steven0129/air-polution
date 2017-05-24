f06a6d79f21a1c5c378402cfd08861114a51fac1
stevengaoismyname <h3584935@gmail.com>
2017-05-25 00:31:01 +0800

    加入事件處理機制

--- air-polution/lib/gui/view/window.py
+++ air-polution/lib/gui/view/window.py
@@ -3,13 +3,18 @@ import tkinter as tk
 class gen:
 	def __init__(self, **kwargs):
 		self.win = tk.Tk()
+		self.widget = {}
 		return super().__init__(**kwargs)
 
 	def setTitle(self, title):
 		self.win.title(title)
 
-	def add(self, signal, text, x, y):
-		_Widget(signal=signal, win=self.win, text=text, x=x, y=y)
+	def add(self, signal, name, text, x, y):
+		widget = _Widget(signal=signal, win=self.win, text=text, x=x, y=y)
+		self.widget[name] = widget.gen()
+
+	def addEventListener(self, key, signal, callback):
+		self.widget[key].bind(signal, callback)
 
 	def run(self):
 		self.win.mainloop()
@@ -18,16 +23,24 @@ class _Widget:
 	def __init__(self, **kwargs):
 		print(kwargs)
 		self.win = kwargs['win']
+		self.signal = kwargs['signal']
+		self.text = kwargs['text']
+		self.x = kwargs['x']
+		self.y = kwargs['y']
+
+	def gen(self):
 		method = ['label', 'button']
-		if kwargs['signal'] == method[0]:
-			self._Label(kwargs['text'], kwargs['x'], kwargs['y'])
-		elif kwargs['signal'] == method[1]:
-			self._Button(kwargs['text'], kwargs['x'], kwargs['y'])
+		if self.signal == method[0]:
+			return self._Label(self.text, self.x, self.y)
+		elif self.signal == method[1]:
+			return self._Button(self.text, self.x, self.y)
 
 	def _Label(self, text, x, y):
 		label = tk.Label(self.win, text=text)
 		label.place(x=x, y=y)
+		return label
 
 	def _Button(self, text, x, y):
 		button = tk.Button(self.win, text=text)
 		button.place(x=x, y=y)
+		return button
--- air-polution/main.py
+++ air-polution/main.py
@@ -1,22 +1,38 @@
-import lib.db as db
-import lib.air as air
-import lib.ML as ML
-import matplotlib.pyplot as plt
-import datetime
+#import lib.db as db
+#import lib.air as air
+#import lib.ML as ML
+#import matplotlib.pyplot as plt
+#import datetime
 
-DB = db.mongo('air', 'element')
-AirFilter = air.filter()
+#DB = db.mongo('air', 'element')
+#AirFilter = air.filter()
 
-PM25List = DB.find({
-    "測站": "新營", "測項": "PM2.5"
-}).sort('日期')
+#PM25List = DB.find({
+#    "測站": "新營", "測項": "PM2.5"
+#}).sort('日期')
 
-timeseries = []
-for PM25 in PM25List:
-    timeseries.extend(AirFilter.dayPolution(PM25))
+#timeseries = []
+#for PM25 in PM25List:
+#    timeseries.extend(AirFilter.dayPolution(PM25))
 
-# plt.plot(timeseries)
-# plt.savefig(str(datetime.datetime.now()) + '.png')
+## plt.plot(timeseries)
+## plt.savefig(str(datetime.datetime.now()) + '.png')
 
-dl = ML.deepLearning(timeseries)
-dl.model1(trainRatio=0.67, epochs=1)
+#dl = ML.deepLearning(timeseries)
+#dl.model1(trainRatio=0.67, epochs=1)
+import lib.gui.view.window as Window
+import tkinter as tk
+from tkinter import messagebox
+
+win = Window.gen()
+win.setTitle('test')
+win.add(signal='label', name='label1', text='test', x=30, y=40)
+win.add(signal='label', name='label2', text='tssss', x=30, y=100)
+win.add(signal='button', name='button1', text='tssss', x=50, y=100)
+
+def show(e):
+	messagebox.showinfo('hello world', 'ddd')
+
+win.addEventListener('button1', '<Button-1>', show)
+
+win.run()
